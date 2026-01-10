bfc54cd21f1d578420ca0dc762e63be35cd8c91c
西井勝巳 <nishiikatsumi@nishiikatsuminoMacBook-Air.local>
2026-01-10 15:35:44 +0900

    課題の実装

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    dangerouslyAllowLocalIP: process.env.NODE_ENV === "development",
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.jp',
-        port: '',
-        pathname: '/**',
-      }
+      { protocol: 'https', hostname: 'placehold.jp' },
+      { protocol: 'https', hostname: 'images.microcms-assets.io' }, // これを追加
     ],
   },
 };
